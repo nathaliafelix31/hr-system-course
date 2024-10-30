02de9a51e19dbf14cb275ebb168124b28acc4189
nathaliafelix31 <nathy.felix.31@gmail.com>
2024-10-30 11:19:31 -0300

    load balancing

--- user-api/src/main/java/br/com/felix/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/felix/userapi/services/impl/UserServiceImpl.java
@@ -4,20 +4,27 @@ import br.com.felix.userapi.domain.User;
 import br.com.felix.userapi.repositories.UserRepository;
 import br.com.felix.userapi.services.UserService;
 import br.com.felix.userapi.services.exceptions.ObjectNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository repository;
+
+    private final UserRepository repository;
+    private final Environment env;
 
 
     @Override
     public User findById(long id) {
+        log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
         return repository.findById(id).orElseThrow(()-> new ObjectNotFoundException("Object not found"));
 
     }
